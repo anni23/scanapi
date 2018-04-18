9bd8fb9ed88e585dbb6f9b9a3b757293d1011181
anni23 <aniruddhrchaturvedi@gmail.com>
2018-04-17 21:44:11 -0700

    Comments revised

--- webapp/scanfile.py
+++ webapp/scanfile.py
@@ -5,7 +5,7 @@ import json
 import hashlib
 api_key = "" #PLEASE ENTER THE API KEY HERE.
 
-#function to compute hash values of the input file.
+#function to compute the hash values of the input file.
 def getHashValues(file_name):
 	#md5.
 	hasher = hashlib.md5()
@@ -27,7 +27,7 @@ def getHashValues(file_name):
 	file_hash_sha256 = hasher.hexdigest().upper() 
 	return file_hash_md5, file_hash_sha1, file_hash_sha256
 
-#function to upload file and return the unique data id.
+#function to upload the file and return the unique data id.
 def uploadFile(file_name):
 	file_upload_url = "https://api.metadefender.com/v2/file"
 	file_upload_headers = {"apikey" : api_key}
@@ -37,30 +37,30 @@ def uploadFile(file_name):
 	data_id = file_upload_jsonres['data_id']
 	return data_id
 
-#function to get scan report by passing the unique data_id obtained by uploading the file.
+#function to get the scan report, by passing the unique data_id obtained by uploading the file.
 def getScanReport(data_id):
 	scan_report_url = "https://api.metadefender.com/v2/file/" + data_id
 	scan_report_headers = {"apikey" : api_key, "file_metadata" : "1"}
 	scan_report_response = requests.get(url = scan_report_url, headers = scan_report_headers)
 	scan_report_jsonres = scan_report_response.json()
-	#check is the progress percent is 100%
+	#check if the progress percent is 100%
 	while(scan_report_jsonres["scan_results"]["progress_percentage"] != 100):
 		scan_report_response = requests.get(url = scan_report_url, headers = scan_report_headers)
 		scan_report_jsonres = scan_report_response.json()
 	return scan_report_jsonres	
 
-#This function is the entry point to the program, it takes in file_name as the argument.
+#This function is the entry point to the program, it takes in file_name as an argument.
 def callScanAPI(file_name):
 	#First we compute hash values of the file.
 	file_hash_md5, file_hash_sha1, file_hash_sha256 = getHashValues(file_name)
 	
-	#Then we call the api to retrieve scan reports using the data hash. 
+	#Then we call the api to retrieve the scan reports using the data hash. 
 	hash_report_url = "https://api.metadefender.com/v2/hash/" + file_hash_sha1
 	headers = {"apikey" : api_key, "file_metadata" : "1"}
 	hash_report_resp = requests.get(url = hash_report_url, headers = headers)
 	hash_report_jsonres = hash_report_resp.json()
 	
-	#Check if the scan report is already available in the cache or its a first time scan. 
+	#Check if the scan report is already available in the cache or is it a first time scan. 
 	if file_hash_sha1 in hash_report_jsonres:    #its a first time scan, upload the file and perform the scan.
 		print("Scan report not available in cache, its a first time scan, it will take a while")
 		data_id = uploadFile(file_name)
